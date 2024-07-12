50ca1347dfb1905ebc820d6c0fb19f37a0b1721c
tatum-deadon <tatumdeadon@gmail.com>
2024-07-12 12:23:46 +0800

    feat: init package

---
+++ nerveboard/__init__.py
@@ -0,0 +1,8 @@
+"""Nerveboard -- Developer productivity dashboard."""
+
+__version__ = "0.1.0"
+
+from nerveboard.analytics import Analytics
+from nerveboard.dashboard import Dashboard
+
+__all__ = ["Analytics", "Dashboard"]
